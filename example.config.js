fa7a8e802376dc33a0a7b542984714b830aa1a7f
Amio <amio.cn@gmail.com>
2018-07-17 15:59:53 +0800

    Fix ocelot redirect 404

--- example.config.js
+++ example.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   '/': 'https://github.com/amio/now-go',
   '/tag': 'Now go, let the legend come back to life!',
-  '/hero': 'https://amio.github.io/now-go-configs/ocelot.jpg',
+  '/hero': 'https://amio.github.io/now-go/ocelot.jpg',
   '*': (req) => {
     if (req.headers.host === 'localhost:3000') {
       return 'Hello from localhost:3000'
